3746df42cc988e55d3377c492843ed1a6eb2b6c6
Matthew Toseland <toad@amphibian.dyndns.org>
2007-05-10 12:04:38 +0000

    More fixes - can deal with patches impacting plugins as well, and separate scripts for renaming vs content
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/scripts@13197 67a373e5-eb02-0410-a15c-ee090a768436

--- verify-whitespace-patch.sh
+++ verify-whitespace-patch.sh
@@ -9,6 +9,8 @@ if [[ -z $FILENAME ]] ; then FILENAME=/home/toad/${REVISION}.diff; fi
 echo Filename: "$FILENAME"
 echo -n "Please enter sed script?"
 read SEDSCRIPT
+echo -n "Please enter rename script?"
+read RENAMESCRIPT
 # Make a temporary directory
 TEMPDIR=`mktemp -d ~/verify-patch-temp-XXXXXXXXXX`
 echo Temporary directory: $TEMPDIR
@@ -21,21 +23,31 @@ cp -a "$WORKINGCOPY" $OLDDIR/
 cd $OLDDIR
 DNAME=`ls`
 echo DNAME = "$DNAME"
-mv */* */.[a-z0-9]* .
-rm -R "$DNAME"
+cd "${OLDDIR}/${DNAME}"
+ant distclean
 svn revert -R .
 svn update -r $OLDREVISION
+cd "$OLDDIR"
+mv "$DNAME" freenet
+cd freenet
+mv src/plugins ..
+cd ../plugins
 ant distclean
+svn revert -R .
+svn update -r $OLDREVISION
+cd ..
 find -iname .svn | xargs rm -R
 rm -R .[a-z0-9]*
 cp -a $OLDDIR/* $NEWDIR
 if [[ -n $SEDSCRIPT ]]; then
 	cd $OLDDIR
 	find . -type f -iname "*.java" | (while read x; do cat "$x" | sed "$SEDSCRIPT" > "$x.1"; mv "$x.1" "$x"; done)
-	find `pwd` -type d | (while read x; do cd "$x"; rename "$SEDSCRIPT" *; done)
 fi
-cd $NEWDIR
-if ! patch -p2 < $FILENAME ; then exit Failed to apply patch ; else echo Applied patch successfully. ; fi
+if [[ -n $RENAMESCRIPT ]]; then
+	find "$OLDDIR" -type d | (while read x; do cd "$x"; rename "$SEDSCRIPT" *; done)
+fi
+cd "$NEWDIR"
+if ! patch -p1 < $FILENAME ; then exit Failed to apply patch ; else echo Applied patch successfully. ; fi
 diff -Nurw "$OLDDIR" "$NEWDIR" > $TEMPDIR/diff-uw
 if [ -s $TEMPDIR/diff-uw ] ; then less $TEMPDIR/diff-uw ; else echo No differences found after compensating for whitespace; fi
 rm -R $TEMPDIR
