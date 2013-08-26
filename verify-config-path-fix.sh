53289ffe6720663207acfcbf6a51aee9970fcb9e
mempo-devel1 <opensource@mempo.org>
2013-08-26 20:36:52 +0200

    No need for /g we execpt only one replacement

--- verify-config-path-fix.sh
+++ verify-config-path-fix.sh
@@ -2,7 +2,7 @@
 
 # Goal: We need to edit ini file to change some relative pateches into absolute path.
 # Purpose: this is needed as part of verify-build for example.
-# Tool: sed.  Will use expressions like: s/releaseDir="\.\./releaseDir="\/home\/fn_verify\/01453/g
+# Tool: sed.  Will use expressions like: s/releaseDir="\.\./releaseDir="\/home\/fn_verify\/01453/
 
 # *** First get absolute path to freenet installation that is 1-level up form location of this script (../)
 this_script_filename=`realpath $0` 
@@ -14,10 +14,10 @@ freenet_dir_escapedsed=`echo "$freenet_dir" | sed -e 's|/|\\\\/|g'`
 
 for varname in "releaseDir" "fredDir" # replacing
 do
-	expr1='s/'; expr2='="\.\./'; expr3='="'; expr4='/g' 
+	expr1='s/'; expr2='="\.\./'; expr3='="'; expr4='/' 
 	expr="${expr1}${varname}${expr2}${varname}${expr3}${freenet_dir_escapedsed}${expr4}"; 
 	sed -e "$expr" ~/.freenetrc > ~/.freenetrc-absolute ; mv ~/.freenetrc-absolute ~/.freenetrc
-	# e.g.: sed -e 's/fredDir="\.\./fredDir="\/home\/fn_verify\/01453/g' /home/fn_verify/.freenetrc
+	# e.g.: sed -e 's/fredDir="\.\./fredDir="\/home\/fn_verify\/01453/' /home/fn_verify/.freenetrc
 done
 
 echo "Fixed the patches, should be absolute now (where needed)"
