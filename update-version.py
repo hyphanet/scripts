974e0ac01b13940cad65d9f277ff2ec6efe14dc9
Steve Dougherty <steve@asksteved.com>
2016-01-21 20:59:36 -0500

    update-version.py: allow running non-interactively

--- update-version.py
+++ update-version.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python2
 import re
 import argparse
 import timeparser
@@ -5,16 +6,22 @@ import datetime
 import calendar
 
 parser = argparse.ArgumentParser(description="""
-Modify Version.java with the given build number, and prompt for when the
-build is mandatory.
+Modify Version.java with the given build number, optionally changing the
+mandatory date.
 """)
 parser.add_argument("build", help="Build number to set", type=int)
 parser.add_argument("path", help="Path to Version.java")
+parser.add_argument("--change-mandatory",
+                    help="Change mandatory information. If specified, --date "
+                         "must also be specified.",
+                    action="store_true")
+parser.add_argument("--version-only", help="Change Version.java, but not the "
+                                           "mandatory build.",
+                    action="store_true")
+parser.add_argument("--date", help="Date the current build goes mandatory in "
+                                   "ISO 8601.",
+                    type=lambda s: datetime.datetime.strptime(s, "%Y-%m-%d"))
 args = parser.parse_args()
-
-with open(args.path) as version_file:
-    version_contents = version_file.readlines()
-    version_contents = "".join(version_contents)
 
 
 def match_number(line):
@@ -33,71 +40,103 @@ new_match = match_number(new_line)
 mandatory_line = "_cal.set( {}, Calendar.{}, {}, 0, 0, 0 );"
 mandatory_match = re.compile(r"_cal\.set\( (\d+), Calendar\.([A-Z]+), (\d+), 0, 0, 0 \);")
 
-build_replacement = build_number_line.format(args.build)
-version_contents = build_match.sub(build_replacement, version_contents)
-print("Set build number to {}.".format(args.build))
+
+def main():
+    if args.version_only and args.change_mandatory:
+        print("--version-only and --change-mandatory cannot both be specified.")
+        exit(1)
+
+    with open(args.path) as version_file:
+        version_contents = version_file.readlines()
+        version_contents = "".join(version_contents)
+
+    # Replace the build number in Version.java.
+    build_replacement = build_number_line.format(args.build)
+    version_contents = build_match.sub(build_replacement, version_contents)
+    print("Set build number to {}.".format(args.build))
+
+    if args.version_only:
+        write_version(version_contents)
+        exit()
+
+    if args.change_mandatory:
+        write_version(update_mandatory(version_contents, args.date))
+        exit()
+
+    write_version(interactive(version_contents))
+    exit()
 
 
-def write_version():
+def write_version(version_contents):
     with open(args.path, "w") as version_file:
         version_file.write(version_contents)
 
 
-def prompt(prompt):
+def interactive(version_contents):
+    if not prompt("Will this build be mandatory?"):
+        write_version(version_contents)
+        exit()
+
+    print("Durations can be things like '1 week' or '5 days'.")
+    today = datetime.datetime.now().date()
+    while True:
+        try:
+            duration_input = raw_input("How long until until mandatory? ")
+            duration = timeparser.parsetimedelta(duration_input)
+            mandatory_date = today + duration
+
+            if prompt("Go mandatory on {}?".format(mandatory_date)):
+                break
+        except EOFError:
+            print("")
+        except ValueError as e:
+            print(e)
+
+    return update_mandatory(version_contents, mandatory_date)
+
+
+def prompt(message):
     while True:
         try:
             # TODO: Python 3 renames raw_input() to input()
-            choice = raw_input(prompt + " [y/n] ")
+            choice = raw_input(message + " [y/n] ")
             return choice.lower() == "y"
         except EOFError:
             pass
 
-if not prompt("Will this build be mandatory?"):
-    write_version()
-    exit()
 
-print("Durations can be things like '1 week' or '5 days'.")
-today = datetime.datetime.now().date()
-while True:
-    try:
-        duration_input = raw_input("How long until until mandatory? ")
-        duration = timeparser.parsetimedelta(duration_input)
-        mandatory_date = today + duration
-
-        if prompt("Go mandatory on {}?".format(mandatory_date)):
-            break
-    except EOFError:
-        print("")
-    except ValueError as e:
-        print(e)
-
-
-def replace(match, value):
+def replace(version_contents, match, value):
     contents, replacements = match.subn(value, version_contents)
     if replacements != 1:
         print("Cannot substitute '{}'".format(value))
         exit(1)
     return contents
 
-previous_new = new_match.search(version_contents)
-if not previous_new:
-    print("Cannot find newLastGoodBuild")
-    exit(1)
 
-previous_new = previous_new.group(1)
-# Previous newLastGoodBuild becomes current oldLastGoodBuild
-current_old = old_line.format(previous_new)
-version_contents = replace(old_match, current_old)
+def update_mandatory(version_contents, mandatory_date):
+    previous_new = new_match.search(version_contents)
+    if not previous_new:
+        print("Cannot find newLastGoodBuild")
+        exit(1)
 
-# Current build becomes newLastGoodBuild
-current_new = new_line.format(args.build)
-version_contents = replace(new_match, current_new)
+    previous_new = previous_new.group(1)
+    # Previous newLastGoodBuild becomes current oldLastGoodBuild
+    current_old = old_line.format(previous_new)
+    version_contents = replace(version_contents, old_match, current_old)
 
-current_mandatory = mandatory_line.format(
-    mandatory_date.year,
-    calendar.month_name[mandatory_date.month].upper(),
-    mandatory_date.day
-)
-version_contents = replace(mandatory_match, current_mandatory)
+    # Current build becomes newLastGoodBuild
+    # TODO: Only when self-mandatory; allow this to be done differently.
+    current_new = new_line.format(args.build)
+    version_contents = replace(version_contents, new_match, current_new)
 
-write_version()
+    current_mandatory = mandatory_line.format(
+        mandatory_date.year,
+        calendar.month_name[mandatory_date.month].upper(),
+        mandatory_date.day
+    )
+    version_contents = replace(version_contents,
+                               mandatory_match, current_mandatory)
+    return version_contents
+
+if __name__ == "__main__":
+    main()
